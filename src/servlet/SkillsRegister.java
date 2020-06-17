0253ce928c714cdd0684ed601c8e9cb4caaaeffc
Aitec <Aitec@Aitec201902017R>
2020-06-17 15:40:59 +0900

    registerとupdateを「スキル・資格で共通」という意味で『Skills』に変更しました。

---
+++ src/servlet/SkillsRegister.java
@@ -0,0 +1,17 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Copyright 2020 FUJITSU SOCIAL SCIENCE LABORATORY LIMITED<br>
+ * システム名：自己紹介システム
+ * クラス概要：スキル資格登録のコントローラクラス
+ */
+
+@WebServlet("/SkillsRegister")
+public class SkillsRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+}
